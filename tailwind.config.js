df0754affdc1baec45a64c0ed83c761df870a142
MasrulSakib <Masrul.Sakib69@gmail.com>
2023-05-10 02:16:51 +0600

    navbar and extra feature added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,15 @@ module.exports = {
     },
     plugins: [],
 }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            display: ["Satisfy", "cursive"],
+        },
+    },
+    plugins: [],
+};
+
